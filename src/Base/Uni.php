03ccf95e1849d0b39de83aff2012cd5be241d4d1
绯末 <famio@qq.com>
2020-01-15 09:39:00 +0800

    更新二进制库

--- src/Base/Uni.php
+++ src/Base/Uni.php
@@ -78,4 +78,60 @@ class Uni
         $return = implode('', $chinese[0]);
         return $return;
     }
+
+    /**
+     * 字符串丢盐加密
+     * @param $string
+     * @param $salt
+     * @return string
+     */
+    static function encryptString($string, $salt)
+    {
+        $base64_string = base64_encode($string);
+        $base64_salt_string = $base64_string . $salt;
+        $base64_second_string = base64_encode($base64_salt_string);
+        $binary_string = self::stringToBinary($base64_second_string);
+        $array = explode(' ', $binary_string);
+        foreach ($array as &$v) {
+            $v = strrev($v);
+        }
+        $result = join(' ', $array);
+        $result = self::binaryToString($result);
+        return $result;
+    }
+
+    /**
+     * 字符串转二进制输出
+     * @param $string
+     * @return string
+     */
+    static function stringToBinary($string)
+    {
+        $array = preg_split('/(?<!^)(?!$)/u', $string);
+        foreach ($array as &$v) {
+            $temp = unpack('H*', $v);
+            $v = base_convert($temp[1], 16, 2);
+            unset($temp);
+        }
+        return join(' ', $array);
+    }
+
+    /**
+     * 二进制字符串转字符串输出
+     * @param $string
+     * @return string
+     */
+    static function binaryToString($string)
+    {
+        $array = explode(' ', $string);
+        foreach ($array as &$v) {
+            $v = pack("H" . strlen(base_convert($v, 2, 16)), base_convert($v, 2, 16));
+        }
+        return join('', $array);
+    }
+
+    static function decryptString($string, $salt)
+    {
+
+    }
 }
