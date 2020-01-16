ff226036ba9697c94874d3ba0168e87b82234e3a
famio <famio@qq.com>
2020-01-16 21:55:22 +0800

    增加了对比两个数组差异值的方法

--- src/Base/Uni.php
+++ src/Base/Uni.php
@@ -80,58 +80,20 @@ class Uni
     }
 
     /**
-     * 字符串丢盐加密
-     * @param $string
-     * @param $salt
-     * @return string
+     * 比较两个数组中的差异值，返回$arrayA比$arrayB中多的值
+     * @param $arrayA
+     * @param $arrayB
+     * @return array
      */
-    static function encryptString($string, $salt)
+    static function diffTwoArray($arrayA, $arrayB)
     {
-        $base64_string = base64_encode($string);
-        $base64_salt_string = $base64_string . $salt;
-        $base64_second_string = base64_encode($base64_salt_string);
-        $binary_string = self::stringToBinary($base64_second_string);
-        $array = explode(' ', $binary_string);
-        foreach ($array as &$v) {
-            $v = strrev($v);
+        $result = array();
+        foreach ($arrayA as $a) {
+            if (!in_array($a, $arrayB)) {
+                array_push($add_ids, $result);
+            }
         }
-        $result = join(' ', $array);
-        $result = self::binaryToString($result);
+        $result = array_unique($result);
         return $result;
-    }
-
-    /**
-     * 字符串转二进制输出
-     * @param $string
-     * @return string
-     */
-    static function stringToBinary($string)
-    {
-        $array = preg_split('/(?<!^)(?!$)/u', $string);
-        foreach ($array as &$v) {
-            $temp = unpack('H*', $v);
-            $v = base_convert($temp[1], 16, 2);
-            unset($temp);
-        }
-        return join(' ', $array);
-    }
-
-    /**
-     * 二进制字符串转字符串输出
-     * @param $string
-     * @return string
-     */
-    static function binaryToString($string)
-    {
-        $array = explode(' ', $string);
-        foreach ($array as &$v) {
-            $v = pack("H" . strlen(base_convert($v, 2, 16)), base_convert($v, 2, 16));
-        }
-        return join('', $array);
-    }
-
-    static function decryptString($string, $salt)
-    {
-
     }
 }
