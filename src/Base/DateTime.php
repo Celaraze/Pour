2ad9937304119c8a219782c7ab2c0e5bf462567e
Famio <famio@qq.com>
2020-04-06 21:24:20 +0800

    增加获取给定日期的时间周期范围的方法

--- src/Base/DateTime.php
+++ src/Base/DateTime.php
@@ -19,4 +19,48 @@ class DateTime
         $s = (strlen($s) == 1) ? '0' . $s : $s;
         return $h . ':' . $s;
     }
+
+    /**
+     * 当前的时间范围 日,周,月,季,年
+     * @param $date
+     * @param string $type
+     * @param int $shift
+     * @return array
+     */
+    public function currentDateTimeRange($date, $type, $shift = 0)
+    {
+        switch ($type) {
+            case 'week':
+                if ($shift != 0) {
+                    $date = date('Y-m-d', strtotime("$shift week"));
+                }
+                $last_day = date("Y-m-d 23:59:59", strtotime("$date Sunday"));
+                $first_day = date("Y-m-d 00:00:00", strtotime("$last_day -6 days"));
+                break;
+            case 'month':
+                if ($shift != 0) {
+                    $date = date('Y-m-d', strtotime("$shift months"));
+                }
+                $first_day = date("Y-m-01 00:00:00", strtotime($date));
+                $last_day = date("Y-m-d 23:59:59", strtotime("$first_day +1 month -1 day"));
+                break;
+            case 'first-half-of-the-year':
+                $first_day = date("Y-01-01 00:00:00", strtotime($date));
+                $last_day = date("Y-06-30 23:59:59", strtotime($date));
+                break;
+            case 'quarter':
+                $season = ceil(date('n', strtotime($date)) / 3);
+                $first_day = date('Y-m-01 00:00:00', mktime(0, 0, 0, ($season - 1) * 3 + 1, 1, date('Y')));
+                $last_day = date('Y-m-t 23:29:29', mktime(0, 0, 0, $season * 3, 1, date('Y')));
+                break;
+            case 'year':
+                $first_day = date("Y-01-01 00:00:00", strtotime($date));
+                $last_day = date("Y-12-31 23:59:59", strtotime($date));
+                break;
+            default:
+                $first_day = date('Y-m-d 00:00:00', strtotime("$shift day"));
+                $last_day = date("Y-m-d 23:59:59", strtotime("$shift day"));
+        }
+        return array($first_day, $last_day);
+    }
 }
